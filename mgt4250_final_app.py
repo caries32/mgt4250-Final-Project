fefadf6a01e59c524b00f7acb3d79d05aa67da72
caries32 <133187234+caries32@users.noreply.github.com>
2023-12-06 23:49:26 -0500

    Update mgt4250_final_app.py

--- mgt4250_final_app.py
+++ mgt4250_final_app.py
@@ -14,7 +14,7 @@ def load_data():
 data = load_data()
 
 # Sidebar for user input
-st.sidebar.header('User Input Features')
+st.sidebar.header('County Select Box')
 selected_county = st.sidebar.selectbox('County', sorted(data['County'].unique()))
 
 # Filtering data based on selection
